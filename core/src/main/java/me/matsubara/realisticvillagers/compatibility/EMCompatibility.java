3b9923f8ea6cae33414f0ad23c67c59083b7998e
aematsubara <correo.agustinmatsubara@gmail.com>
2024-07-06 16:39:51 -0300

    Fix incompatibility issue with EliteMobs.

--- core/src/main/java/me/matsubara/realisticvillagers/compatibility/EMCompatibility.java
+++ core/src/main/java/me/matsubara/realisticvillagers/compatibility/EMCompatibility.java
@@ -1,16 +1,16 @@
 package me.matsubara.realisticvillagers.compatibility;
 
-import com.magmaguy.elitemobs.entitytracker.EntityTracker;
+import com.magmaguy.elitemobs.tagger.PersistentTagger;
 import org.bukkit.entity.Villager;
 
 public class EMCompatibility implements Compatibility {
 
     @Override
     public boolean shouldTrack(Villager villager) {
-        return !EntityTracker.isNPCEntity(villager)
-                && !EntityTracker.isProjectileEntity(villager)
-                && !EntityTracker.isEliteMob(villager)
-                && !EntityTracker.isSuperMob(villager)
-                && !EntityTracker.isVisualEffect(villager);
+        return !PersistentTagger.isNPC(villager)
+                && !PersistentTagger.isEliteProjectile(villager)
+                && !PersistentTagger.isEliteEntity(villager)
+                && !PersistentTagger.isSuperMob(villager)
+                && !PersistentTagger.isVisualEffect(villager);
     }
 }
