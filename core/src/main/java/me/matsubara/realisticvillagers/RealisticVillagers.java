98af4acff853dcb88814f67cfaecf52dadf6a67e
Nikoloz <147946324+SSStarku@users.noreply.github.com>
2025-02-15 12:05:46 +0100

    Update RealisticVillagers.java

--- core/src/main/java/me/matsubara/realisticvillagers/RealisticVillagers.java
+++ core/src/main/java/me/matsubara/realisticvillagers/RealisticVillagers.java
@@ -804,7 +804,7 @@ public final class RealisticVillagers extends JavaPlugin {
         Plugin marriage = Bukkit.getServer().getPluginManager().getPlugin("MarriageMaster");
         if (marriage == null) return false;
         return getCompatibilityManager().marriedPlayer(player);
-        System.out.println("ismarried")
+        System.out.println("ismarried");
     }
     public boolean isMarried(@NotNull Player player) {
         String partner = player.getPersistentDataContainer().get(marriedWith, PersistentDataType.STRING);
